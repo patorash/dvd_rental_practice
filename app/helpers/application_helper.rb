dd5831fc26b8f2360ee7834e3ca5499cb8df19d6
patorash <chariderpato@gmail.com>
2018-08-28 16:47:09 +0900

    集約関数について追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def table_of_contents(menus)
+    content_tag :ul do
+      menus.map do |menu|
+        if menu.end_with? '?'
+          content_tag :li do
+            concat(link_to menu, "##{menu.gsub(/\?/, '')}")
+          end
+        else
+          content_tag :li do
+            concat(link_to menu, "##{menu}")
+          end
+        end
+      end.inject(:+)
+    end
+  end
+
 end
